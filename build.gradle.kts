3a78b48373c2a66d5de71e27caa0f70c1e223024
ImDaMilan <imdamilan333@gmail.com>
2023-09-27 13:52:14 +0200

    Fixed dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val ktor_version = "2.3.4"
+val kotlin_version = "1.9.10"
 
 plugins {
     kotlin("jvm") version "1.9.10"
@@ -27,7 +26,6 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
