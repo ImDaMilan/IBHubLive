e29890c70ea5d7ab52e7d95bfc31fa6d725e341a
ImDaMilan <imdamilan333@gmail.com>
2023-09-27 14:37:18 +0200

    Port setting 2

--- src/main/kotlin/com/imdamilan/Application.kt
+++ src/main/kotlin/com/imdamilan/Application.kt
@@ -7,7 +7,7 @@ import io.ktor.server.netty.*
 import kotlinx.serialization.Serializable
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 10000, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
